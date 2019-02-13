55c4f475af556164e18c46f940237c423a2d5d20
Jeff Benton <sikletz@gmail.com>
2019-02-13 22:31:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
   @@all = []
-  attr_reader :species, :pets
-  attr_accessor :name
+  attr_reader :species
+  attr_accessor :name, :pets
   def initialize(species)
     @species = species
     @pets = {
