41d8ddd965a6c011fd3f6ac46a97f22ce309ea7f
Jeff Benton <sikletz@gmail.com>
2019-02-13 19:22:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
-  # code goes here
+  @@all = []
+  attr_reader :species
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
 end
